ff945fe25ab332f7dd346b15da6dd092d175d872
David E. Wheeler <david@justatheory.com>
2011-01-13 14:27:39 -0800

    Increment version to 3.25.

--- lib/TAP/Parser/SourceHandler/pgTAP.pm
+++ lib/TAP/Parser/SourceHandler/pgTAP.pm
@@ -9,7 +9,7 @@ use TAP::Parser::Iterator::Process ();
 @ISA = qw(TAP::Parser::SourceHandler);
 TAP::Parser::IteratorFactory->register_handler(__PACKAGE__);
 
-our $VERSION = '3.24';
+our $VERSION = '3.25';
 
 =head1 Name
 
