3d741f9162178be8686944662805afd118796c99
David E. Wheeler <david.wheeler@iovation.com>
2018-11-27 22:26:46 -0500

    Add Postgres environment variables.
    And fix the image name.

--- dist/docker-pg_prove.sh
+++ dist/docker-pg_prove.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-PGPROVE_IMAGE=${PGPROVE_IMAGE:=pg_prove/pg_prove:latest}
+PGPROVE_IMAGE=${PGPROVE_IMAGE:=itheory/pg_prove:latest}
+
+passenv=()
+
+for var in \
+    PGUSER PGPASSWORD PGHOST PGHOSTADDR PGPORT PGDATABASE PGSERVICE \
+    PGOPTIONS PGSSLMODE PGREQUIRESSL PGSSLCOMPRESSION PGREQUIREPEER \
+    PGKRBSRVNAME PGKRBSRVNAME PGGSSLIB PGCONNECT_TIMEOUT PGCLIENTENCODING \
+    PGTARGETSESSIONATTRS
+do
+    if [ ! -z "${!var}" ]; then
+       passenv+=("-e" "$var=${!var}")
+    fi
+done
 
 docker run -it --rm --network host \
     --mount "type=bind,src=$(pwd),dst=/repo" \
     --mount "type=bind,src=$HOME,dst=/root" \
-    pg_prove $@
+    "${passenv[@]}" "$PGPROVE_IMAGE"  $@
